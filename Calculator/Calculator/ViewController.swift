c1c377e42781f061eaaee4e77d0ef846e01081e1
Yu Liu <liuyujack2003@gmail.com>
2015-09-15 12:16:21 +0100

    add Calculator project

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Yu Liu on 15/09/2015.
+//  Copyright (c) 2015 Yu Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
