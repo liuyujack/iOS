e584462134e98b86849c18572499ef6fc89fcebf
Yu Liu <liuyujack2003@gmail.com>
2015-09-15 15:46:14 +0100

    SU CS193p :Lecture 2

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
     
-    var userIsInMiddleOfTypingANumber: Bool = false
+    var userIsInMiddleOfTypingANumber = false
 
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -23,6 +23,55 @@ class ViewController: UIViewController {
             userIsInMiddleOfTypingANumber = true
         }
         
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "×": performOperation{$0 * $1}
+        case "÷": performOperation{$1 / $0}
+        case "+": performOperation{$0 + $1}
+        case "−": performOperation{$1 - $0}
+        case "√": performOperationOne{sqrt($0)}
+        default: break
+            
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double){
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperationOne(operation: Double -> Double){
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInMiddleOfTypingANumber = false
+        operandStack.append(displayValue);
+        println("operandStack = \(operandStack)")
+        //display.text = "0"
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue;
+        }
+        set {
+            display.text! = "\(newValue)"
+            userIsInMiddleOfTypingANumber = false
+        }
     }
 }
 
