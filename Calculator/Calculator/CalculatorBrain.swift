b14f8428f7e34aa24635c43f295ed16b615b940f
Yu Liu <liuyujack2003@gmail.com>
2015-09-16 16:46:41 +0100

    Start of Lecture 3

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+class CalculatorBrain
+{
+    enum Op{
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+    }
+    
+    var opStack = [Op]()
+    
+//    var knownOps = Dictionary<String,Op>()
+    var knownOps = [String:Op]()
+    
+    init() {
+    }
+    
+    func pushOperand(operand: Double) {
+        opStack.append(Op.Operand(operand))
+    }
+    
+    func performOperation(symbol: String) {
+        
+    }
+}
