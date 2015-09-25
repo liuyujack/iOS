8a9c135fc5b4e0332a3265fff611400f0df1f42f
Yu Liu <liuyujack2003@gmail.com>
2015-09-25 13:42:13 +0100

    After Lecture 5

---
+++ Happiness/Happiness/HapplinessViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HapplinessViewController.swift
+//  Happiness
+//
+//  Created by Yu Liu on 25/09/2015.
+//  Copyright © 2015 Yu Liu. All rights reserved.
+//
+
+import UIKit
+
+class HapplinessViewController: UIViewController {
+
+    
+
+}
